b250f1ea81ee7e9f712b208357d770e3d07a4c09
SHyeonCoding <119930129+SHyeonCoding@users.noreply.github.com>
2023-02-17 21:19:21 +0900

    [Bronze V] Title: 長方形 (Rectangle), Time: 120 ms, Memory: 14188 KB -BaekjoonHub

---
+++ 백준/Bronze/27323. 長方形 （Rectangle）/長方形 （Rectangle）.java
@@ -0,0 +1,14 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int a = Integer.parseInt(br.readLine());
+        int b = Integer.parseInt(br.readLine());
+
+        sb.append(a * b);
+        System.out.println(sb);
+    }
+}
