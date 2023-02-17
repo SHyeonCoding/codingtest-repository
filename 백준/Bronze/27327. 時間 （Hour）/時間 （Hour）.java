c9f0908af0c922dc890f9fe53c1d18678326cf42
SHyeonCoding <119930129+SHyeonCoding@users.noreply.github.com>
2023-02-17 21:20:22 +0900

    [Bronze V] Title: 時間 (Hour), Time: 120 ms, Memory: 14072 KB -BaekjoonHub

---
+++ 백준/Bronze/27327. 時間 （Hour）/時間 （Hour）.java
@@ -0,0 +1,13 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int n = Integer.parseInt(br.readLine());
+
+        sb.append(n * 24);
+        System.out.println(sb);
+    }
+}
