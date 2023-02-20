be2a5eca49f80a13f284244c1b62d898c5ce9d8a
SHyeonCoding <119930129+SHyeonCoding@users.noreply.github.com>
2023-02-20 20:28:30 +0900

    [Bronze III] Title: No Brainer, Time: 120 ms, Memory: 14008 KB -BaekjoonHub

---
+++ 백준/Bronze/4562. No Brainer/No Brainer.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int n = Integer.parseInt(br.readLine());
+
+        for(int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int num1 = Integer.parseInt(st.nextToken());
+            int num2 = Integer.parseInt(st.nextToken());
+
+            if(num1 < num2) sb.append("NO BRAINS\n");
+            else sb.append("MMM BRAINS\n");
+        }
+
+        System.out.println(sb);
+    }
+}
