8d65edb2bd203c3ff7eb68a9175e61fc6d414bb6
SHyeonCoding <119930129+SHyeonCoding@users.noreply.github.com>
2023-02-22 21:33:40 +0900

    [Bronze II] Title: 16진수, Time: 124 ms, Memory: 14228 KB -BaekjoonHub

---
+++ 백준/Bronze/1550. 16진수/16진수.java
@@ -0,0 +1,14 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        String s = br.readLine();
+
+        sb.append(Integer.parseInt(s, 16));
+
+        System.out.println(sb);
+    }
+}
