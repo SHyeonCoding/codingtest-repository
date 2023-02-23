5d74e554921e7ad8478e4bae19df112421cd6650
SHyeonCoding <119930129+SHyeonCoding@users.noreply.github.com>
2023-02-23 20:22:33 +0900

    [Bronze IV] Title: Tri-du, Time: 124 ms, Memory: 14184 KB -BaekjoonHub

---
+++ 백준/Bronze/13597. Tri－du/Tri－du.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int num1 = Integer.parseInt(st.nextToken());
+        int num2 = Integer.parseInt(st.nextToken());
+
+        sb.append(Math.max(num1, num2));
+
+        System.out.println(sb);
+    }
+}
