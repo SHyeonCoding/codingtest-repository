ce34ae7c66d8b31bf424a1082742f14ff64f8582
SHyeonCoding <119930129+SHyeonCoding@users.noreply.github.com>
2023-02-27 21:31:37 +0900

    [Bronze IV] Title: 과자, Time: 128 ms, Memory: 14256 KB -BaekjoonHub

---
+++ 백준/Bronze/10156. 과자/과자.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int k = Integer.parseInt(st.nextToken());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        if(k * n > m) sb.append(k * n - m);
+        else sb.append(0);
+
+        System.out.println(sb);
+    }
+}
