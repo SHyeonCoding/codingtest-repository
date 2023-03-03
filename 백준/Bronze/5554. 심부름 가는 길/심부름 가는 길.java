28176e465d162d83362688334e042f1813b9b9ed
SHyeonCoding <119930129+SHyeonCoding@users.noreply.github.com>
2023-03-03 20:41:54 +0900

    [Bronze IV] Title: 심부름 가는 길, Time: 120 ms, Memory: 14100 KB -BaekjoonHub

---
+++ 백준/Bronze/5554. 심부름 가는 길/심부름 가는 길.java
@@ -0,0 +1,18 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int a = Integer.parseInt(br.readLine());
+        int b = Integer.parseInt(br.readLine());
+        int c = Integer.parseInt(br.readLine());
+        int d = Integer.parseInt(br.readLine());
+        int sec = a + b + c + d;
+
+        sb.append(sec / 60).append("\n").append(sec % 60);
+
+        System.out.println(sb);
+    }
+}
