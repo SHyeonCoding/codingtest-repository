50c466be3a7da29c01bcba89a6c3fe364287083c
SHyeonCoding <119930129+SHyeonCoding@users.noreply.github.com>
2023-03-05 11:51:01 +0900

    [Bronze IV] Title: 試験 (Exam), Time: 120 ms, Memory: 14352 KB -BaekjoonHub

---
+++ 백준/Bronze/18411. 試験 （Exam）/試験 （Exam）.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int[] arr = new int[3];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < arr.length; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+
+        sb.append(arr[2] + arr[1]);
+        System.out.println(sb);
+    }
+}
