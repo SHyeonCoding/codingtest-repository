5977987cf5a1422716df2f3dba14c48eae90b7cd
SHyeonCoding <119930129+SHyeonCoding@users.noreply.github.com>
2023-03-11 09:07:54 +0900

    [Bronze IV] Title: Buying in Bulk, Time: 168 ms, Memory: 15796 KB -BaekjoonHub

---
+++ 백준/Bronze/26332. Buying in Bulk/Buying in Bulk.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int n = Integer.parseInt(br.readLine());
+
+        for(int i = 0; i < n; i ++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            sb.append(a).append(" ").append(b).append("\n");
+            sb.append(b * a - (a - 1) * 2).append("\n");
+        }
+
+        System.out.println(sb);
+    }
+}
