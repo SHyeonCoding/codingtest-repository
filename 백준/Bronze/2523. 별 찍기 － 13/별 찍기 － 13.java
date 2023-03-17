d37965a24a486fbd759a3e28829bbc2e79535782
SHyeonCoding <119930129+SHyeonCoding@users.noreply.github.com>
2023-03-17 19:18:46 +0900

    [Bronze III] Title: 별 찍기 - 13, Time: 128 ms, Memory: 14232 KB -BaekjoonHub

---
+++ 백준/Bronze/2523. 별 찍기 － 13/별 찍기 － 13.java
@@ -0,0 +1,20 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int n = Integer.parseInt(br.readLine());
+
+        for(int i = 1; i <= n; i++) {
+            sb.append("*".repeat(i)).append("\n");
+        }
+
+        for(int i = 1; i < n; i++) {
+            sb.append("*".repeat(n - i)).append("\n");
+        }
+
+        System.out.println(sb);
+    }
+}
