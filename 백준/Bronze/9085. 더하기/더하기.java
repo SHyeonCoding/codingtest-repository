13e6e8753a25d91b3e8174459c75007f0b8a460e
SHyeonCoding <119930129+SHyeonCoding@users.noreply.github.com>
2023-03-23 20:50:11 +0900

    [Bronze III] Title: 더하기, Time: 116 ms, Memory: 14064 KB -BaekjoonHub

---
+++ 백준/Bronze/9085. 더하기/더하기.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int n = Integer.parseInt(br.readLine());
+
+        for(int i = 0; i < n; i++) {
+            int sum = 0;
+            int x = Integer.parseInt(br.readLine());
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+            while(st.hasMoreTokens()) {
+                sum += Integer.parseInt(st.nextToken());
+            }
+
+            sb.append(sum).append("\n");
+        }
+
+        System.out.println(sb);
+    }
+}
