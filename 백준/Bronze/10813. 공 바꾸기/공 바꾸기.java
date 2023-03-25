da1c4792df35e5681e23798eee4931a4d3d738fe
SHyeonCoding <119930129+SHyeonCoding@users.noreply.github.com>
2023-03-25 10:56:16 +0900

    [Bronze II] Title: 공 바꾸기, Time: 128 ms, Memory: 14272 KB -BaekjoonHub

---
+++ 백준/Bronze/10813. 공 바꾸기/공 바꾸기.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int[] arr = new int[n];
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = i + 1;
+        }
+
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int temp = arr[a - 1];
+            arr[a - 1] = arr[b - 1];
+            arr[b - 1] = temp;
+        }
+
+        for (int i : arr) {
+            sb.append(i).append(" ");
+        }
+
+        System.out.println(sb);
+    }
+}
