35f86f46be9f1dcad491bb513f9b1daae253181d
SHyeonCoding <119930129+SHyeonCoding@users.noreply.github.com>
2023-04-01 10:47:22 +0900

    [Bronze IV] Title: Population, Time: 120 ms, Memory: 13988 KB -BaekjoonHub

---
+++ 백준/Bronze/26561. Population/Population.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int n = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            sb.append(a + (b / 4 - b / 7)).append("\n");
+        }
+
+        System.out.println(sb);
+    }
+}
