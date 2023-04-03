7136029d4c0d14a3d1ff85276ae51fe92a450272
SHyeonCoding <119930129+SHyeonCoding@users.noreply.github.com>
2023-04-03 16:53:49 +0900

    [Bronze IV] Title: 帰省 (Homecoming), Time: 124 ms, Memory: 14152 KB -BaekjoonHub

---
+++ 백준/Bronze/24072. 帰省 （Homecoming）/帰省 （Homecoming）.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+
+        if(a <= c && b > c) sb.append(1);
+        else sb.append(0);
+
+        System.out.println(sb);
+    }
+}
