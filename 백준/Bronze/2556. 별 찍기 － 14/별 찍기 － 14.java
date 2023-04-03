7b5d1c461c158d6815d27c6d79a497a2ee786e02
SHyeonCoding <119930129+SHyeonCoding@users.noreply.github.com>
2023-04-03 17:00:15 +0900

    [Bronze V] Title: 별 찍기 - 14, Time: 120 ms, Memory: 14228 KB -BaekjoonHub

---
+++ 백준/Bronze/2556. 별 찍기 － 14/별 찍기 － 14.java
@@ -0,0 +1,16 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int n = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < n; i++) {
+            sb.append("*".repeat(n)).append("\n");
+        }
+
+        System.out.println(sb);
+    }
+}
