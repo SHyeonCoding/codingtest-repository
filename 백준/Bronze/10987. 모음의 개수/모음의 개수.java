7c45c040c9eade2339f171ef13dcc781a09b9624
SHyeonCoding <119930129+SHyeonCoding@users.noreply.github.com>
2023-04-06 10:22:52 +0900

    [Bronze III] Title: 모음의 개수, Time: 124 ms, Memory: 14248 KB -BaekjoonHub

---
+++ 백준/Bronze/10987. 모음의 개수/모음의 개수.java
@@ -0,0 +1,19 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        String str = br.readLine();
+        int cnt = 0;
+
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') cnt++;
+        }
+
+        sb.append(cnt);
+        System.out.println(sb);
+    }
+}
