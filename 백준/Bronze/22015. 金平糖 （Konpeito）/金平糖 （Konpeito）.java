8aa25eb50c69e25ea3980240af3de658a28382e2
SHyeonCoding <119930129+SHyeonCoding@users.noreply.github.com>
2023-04-10 10:29:37 +0900

    [Bronze IV] Title: 金平糖 (Konpeito), Time: 128 ms, Memory: 14224 KB -BaekjoonHub

---
+++ 백준/Bronze/22015. 金平糖 （Konpeito）/金平糖 （Konpeito）.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+
+        int max = Math.max(a, Math.max(b, c));
+
+        sb.append((max - a) + (max - b) + (max - c));
+        System.out.println(sb);
+    }
+}
