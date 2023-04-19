9ed1022f7efefe2853086ef1834dcd39fff9c5ab
SHyeonCoding <119930129+SHyeonCoding@users.noreply.github.com>
2023-04-19 19:59:43 +0900

    [Bronze I] Title: 약수, Time: 128 ms, Memory: 14332 KB -BaekjoonHub

---
+++ 백준/Bronze/1037. 약수/약수.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+
+        sb.append(arr[0] * arr[n - 1]);
+        System.out.println(sb);
+    }
+}
