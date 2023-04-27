826b6e2e5d33e55057133572b3c196fb24d6fd4f
SHyeonCoding <119930129+SHyeonCoding@users.noreply.github.com>
2023-04-27 18:46:30 +0900

    [Bronze V] Title: 초코바, Time: 124 ms, Memory: 14328 KB -BaekjoonHub

---
+++ 백준/Bronze/27959. 초코바/초코바.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        if(n * 100 >= m) sb.append("Yes");
+        else sb.append("No");
+
+        System.out.println(sb);
+    }
+}
