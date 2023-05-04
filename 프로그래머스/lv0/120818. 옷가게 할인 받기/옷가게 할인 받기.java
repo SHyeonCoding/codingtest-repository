cd7a1fd6887475aeb16dabb39d9e906b53f7471a
SHyeonCoding <119930129+SHyeonCoding@users.noreply.github.com>
2023-05-04 21:23:05 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.02 ms, Memory: 79.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120818. 옷가게 할인 받기/옷가게 할인 받기.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int solution(int price) {
+        if(price >= 500000) return price * 80 / 100 ;
+        else if(price >= 300000) return price * 90 / 100;
+        else if(price >= 100000) return price * 95 / 100;
+        else return price;
+    }
+}
