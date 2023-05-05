b2af113a3fafe2209529c26826c1f196d5814254
SHyeonCoding <119930129+SHyeonCoding@users.noreply.github.com>
2023-05-05 11:45:00 +0900

    [unrated] Title: 카운트 업, Time: 0.02 ms, Memory: 72.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181920. 카운트 업/카운트 업.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int[] solution(int start, int end) {
+        int[] answer = new int[end - start + 1];
+        
+        for(int i = start; i <= end; i++) {
+            answer[i - start] = i;
+        }
+        
+        return answer;
+    }
+}
