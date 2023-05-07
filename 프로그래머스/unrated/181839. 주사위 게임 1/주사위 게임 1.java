76e4fbe9aa22e673de4f7139af41acc2c76989d5
SHyeonCoding <119930129+SHyeonCoding@users.noreply.github.com>
2023-05-07 10:12:38 +0900

    [unrated] Title: 주사위 게임 1, Time: 0.03 ms, Memory: 77.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181839. 주사위 게임 1/주사위 게임 1.java
@@ -0,0 +1,7 @@
+class Solution {
+    public int solution(int a, int b) {
+        if(a % 2 == 1 && b % 2 == 1) return (int)(Math.pow(a, 2) + Math.pow(b, 2));
+        if(a % 2 == 0 && b % 2 == 0) return Math.abs(a - b);
+        return 2 * (a + b);
+    }
+}
