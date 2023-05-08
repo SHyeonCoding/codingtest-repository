8033931d2fd6a10cd24b0e98b3969d3b2ec8edbd
SHyeonCoding <119930129+SHyeonCoding@users.noreply.github.com>
2023-05-08 20:29:27 +0900

    [unrated] Title: 접두사인지 확인하기, Time: 0.03 ms, Memory: 75 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181906. 접두사인지 확인하기/접두사인지 확인하기.java
@@ -0,0 +1,7 @@
+class Solution {
+    public int solution(String my_string, String is_prefix) {
+        if(my_string.length() < is_prefix.length()) return 0;
+        if(my_string.substring(0, is_prefix.length()).equals(is_prefix)) return 1;
+        return 0;
+    }
+}
