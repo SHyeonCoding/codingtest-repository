d32c217f05c1be8aeb0d9d32c987075b49d47f20
SHyeonCoding <119930129+SHyeonCoding@users.noreply.github.com>
2023-05-13 10:39:05 +0900

    [unrated] Title: 문자 개수 세기, Time: 0.02 ms, Memory: 73.7 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181902. 문자 개수 세기/문자 개수 세기.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int[] solution(String my_string) {
+        int[] answer = new int[52];
+        
+        for(int i = 0; i < my_string.length(); i++) {
+            if(Character.isUpperCase(my_string.charAt(i))) answer[my_string.charAt(i) - 'A']++;
+            else answer[my_string.charAt(i) - 'a' + 26]++;
+        }
+        
+        return answer;
+    }
+}
