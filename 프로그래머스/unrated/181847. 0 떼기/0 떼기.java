f8656c5108ba7878df5532b8cee182e46a9a00c6
SHyeonCoding <119930129+SHyeonCoding@users.noreply.github.com>
2023-05-15 20:41:23 +0900

    [unrated] Title: 0 떼기, Time: 0.01 ms, Memory: 78 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181847. 0 떼기/0 떼기.java
@@ -0,0 +1,9 @@
+class Solution {
+    public String solution(String n_str) {
+        if(n_str.startsWith("0")) {
+            n_str = n_str.substring(1);
+            return solution(n_str);
+        } 
+        return n_str;
+    }
+}
