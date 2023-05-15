345629f954c08b263f78a3ba8e4bebc48e41da87
SHyeonCoding <119930129+SHyeonCoding@users.noreply.github.com>
2023-05-15 20:49:03 +0900

    [level 0] Title: 인덱스 바꾸기, Time: 1.87 ms, Memory: 74.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120895. 인덱스 바꾸기/인덱스 바꾸기.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String solution(String my_string, int num1, int num2) {
+        String[] str = my_string.split("");
+        String answer = "";
+        
+        String temp = str[num1];
+        str[num1] = str[num2];
+        str[num2] = temp;
+        
+        for(String s : str) {
+            answer += s;
+        }
+        
+        return answer;
+    }
+}
