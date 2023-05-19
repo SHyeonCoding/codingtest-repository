aa0d6c59a40ca70428d9d1f2f041dd412b843ac1
SHyeonCoding <119930129+SHyeonCoding@users.noreply.github.com>
2023-05-19 20:37:09 +0900

    [unrated] Title: 커피 심부름, Time: 0.03 ms, Memory: 78.9 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181837. 커피 심부름/커피 심부름.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int solution(String[] order) {
+        int answer = 0;
+        
+        for(String s : order) {
+            if(s.contains("cafelatte")) answer += 5000;
+            else answer += 4500;
+        }
+        
+        return answer;
+    }
+}
