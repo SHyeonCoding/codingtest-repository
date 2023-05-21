3dfcbeaf2d6440c0c0c5d1d18e66238dd978c05c
SHyeonCoding <119930129+SHyeonCoding@users.noreply.github.com>
2023-05-21 13:03:41 +0900

    [level 0] Title: 합성수 찾기, Time: 0.01 ms, Memory: 78 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120846. 합성수 찾기/합성수 찾기.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        
+        for(int i = 4; i <= n; i++) {
+            int cnt = 0;
+            for(int j = 2; j <= i; j++) {
+                if(i % j == 0) cnt++;
+            }
+            
+            if(cnt >= 2) answer++;
+        }
+        
+        return answer;
+    }
+}
