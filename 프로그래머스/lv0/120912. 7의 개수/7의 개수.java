2cdc515d19f0155144b5268a566f24293aa6972c
SHyeonCoding <119930129+SHyeonCoding@users.noreply.github.com>
2023-06-07 15:37:25 +0900

    [level 0] Title: 7의 개수, Time: 0.02 ms, Memory: 75.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120912. 7의 개수/7의 개수.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(int[] array) {
+        int answer = 0;
+        
+        for(int n : array) {
+            while(n > 0) {
+                if(n % 10 == 7) answer++;
+                n /= 10;
+            }
+        }
+        
+        return answer;
+    }
+}
